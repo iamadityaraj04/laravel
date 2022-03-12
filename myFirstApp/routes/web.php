88ebc5a161c0d39d5207dd408dddf6f9bb477dc9
iamadityaraj04 <adityarohitraj@gmail.com>
2022-03-12 19:42:27 +0530

    laravel

---
+++ myFirstApp/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//here test is url 
+//get method is used to represent
+Route::get('/test',function(){
+    return "Hi! Aditya. Welcome to Laravel.";
+});
